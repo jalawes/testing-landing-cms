fc02c831e78cffbece6aff585682df78e9ba1cfa
Jamal Alawes <jamal.alawes@gmail.com>
2021-03-02 18:11:23 -0800

    styles form

--- components/posts/posts.vue
+++ components/posts/posts.vue
@@ -1,44 +1,32 @@
 <template>
-  <ul v-if="posts.length > 0" class="cards">
-    <li
-      v-for="(post, index) in posts"
-      :key="index"
-    >
-      <nuxt-link
-        :to="`${postType}/${post.slug}`"
-        class="card card--clickable"
-      >
-        <template v-if="postType === 'projects'">
-          <span class="flex-1">
-            <h6 class="inline-block py-1 px-2 mr-1 bg-gray text-white text-sm font-medium rounded-sm">{{ post.category }}</h6>
-            <h3 class="card-title">{{ post.title }}</h3>
-            <p class="mt-2">{{ post.description }}</p>
-          </span>
-          <img
-            v-if="post.cover"
-            class="cover-image"
-            :src="post.cover"
-          >
-        </template>
-
-        <template v-else>
-          <span class="w-full">
-            <span class="flex justify-between align-baseline">
-              <h3 class="card-title">{{ post.title }}</h3>
-              <h6
-                v-if="post.createdAt"
-                class="self-start inline-block mt-0 py-1 px-2 bg-gray text-white text-base font-medium rounded-sm whitespace-no-wrap"
-              >{{ formatDate(post.createdAt) }}</h6>
-            </span>
-            <p class="mt-2">{{ post.description }}</p>
-          </span>
-        </template>
-      </nuxt-link>
-    </li>
-  </ul>
-  <p v-else class="max-w-5xl mx-auto">
-    {{ amount > 1 ? 'Posts not found' : 'Post not found' }}
-  </p>
+  <section class="text-gray-600">
+    <div class="py-12 mx-auto" v-if="posts.length > 0">
+      <div class="flex flex-wrap -mx-4 -my-8">
+        <nuxt-link :to="`${postType}/${post.slug}`" v-for="(post, index) in posts" :key="index" class="py-8 px-4 lg:w-1/3 rounded bg-white">
+          <div class="h-full flex items-start">
+            <div class="w-12 flex-shrink-0 flex flex-col text-center leading-none">
+              <span class="text-indigo-500 pb-2 mb-2 border-b-2 border-gray-200">{{ getMonth(post.createdAt) }}</span>
+              <span class="font-medium text-lg text-gray-800 title-font leading-none">{{ getDate(post.createdAt) }}</span>
+            </div>
+            <div class="flex-grow space-x-4">
+              <h2 class="tracking-widest text-xs title-font font-medium text-indigo-500 mb-1">{{ post.category }}</h2>
+              <h1 class="title-font text-xl font-medium text-gray-900 mb-3">{{ post.title }}</h1>
+              <p class="leading-relaxed mb-5">{{ post.description }}</p>
+              <a class="text-indigo-500 inline-flex items-center mt-4">Learn More
+                <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                  <path d="M5 12h14"></path>
+                  <path d="M12 5l7 7-7 7"></path>
+                </svg>
+              </a>
+            </div>
+          </div>
+        </nuxt-link>
+      </div>
+    </div>
+    <p v-else class="max-w-5xl mx-auto">
+      {{ amount > 1 ? 'Posts not found' : 'Post not found' }}
+    </p>
+  </section>
 </template>
 
 <script>
@@ -73,6 +61,14 @@
       this.posts = await this.fetchPosts();
     },
     methods: {
+      getDate(dateString) {
+        const date = new Date(dateString)
+        return date.getDate();
+      },
+      getMonth(dateString) {
+        const date = new Date(dateString)
+        return date.toLocaleString('default', { month: 'long' }).substr(0, 2);
+      },
       formatDate(dateString) {
         const date = new Date(dateString)
         return date.toLocaleDateString(process.env.lang) || ''
