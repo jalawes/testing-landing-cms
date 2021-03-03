c73442e490bce74115f44c9ba6b7418bcaab1790
Jamal Alawes <jamal.alawes@gmail.com>
2021-03-02 17:45:47 -0800

    removes recaptcha

--- pages/index.vue
+++ pages/index.vue
@@ -16,11 +16,7 @@
 
     <section class="text-gray-600 body-font relative">
       <div class="container py-6 mx-auto">
-        <form netlify @submit.prevent="sendForm" name="example-form" method="POST" data-netlify="true">
-          <input type="hidden" name="form-name" value="example-form">
-          <div class="container mx-auto max-w-2xl">
-            <div data-netlify-recaptcha="true" />
-          </div>
+        <form netlify name="example-form" method="POST" data-netlify-honeypot="bot-field">
           <div class="flex flex-col text-center w-full mb-12">
             <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Contact Us</h1>
             <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify.</p>
@@ -33,7 +29,7 @@
               <div class="p-2 w-1/2">
                 <div class="relative">
                   <label for="name" class="leading-7 text-sm text-gray-600">Name</label>
-                  <input type="text" id="name" name="name" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" v-model="form.name">
+                  <input required autofocus="true" type="text" id="name" name="name" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" v-model="form.name">
                 </div>
               </div>
             </div>
@@ -43,19 +39,19 @@
               <div class="p-2 w-1/2">
                 <div class="relative">
                   <label for="email" class="leading-7 text-sm text-gray-600">Email</label>
-                  <input type="email" id="email" name="email" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" v-model="form.email">
+                  <input required type="email" id="email" name="email" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" v-model="form.email">
                 </div>
               </div>
               <div class="p-2 w-1/2">
                 <div class="relative">
                   <label for="postalCode" class="leading-7 text-sm text-gray-600">Postal Code</label>
-                  <input type="text" id="postalCode" name="postalCode" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" v-model="form.postalCode">
+                  <input required type="text" id="postalCode" name="postalCode" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" v-model="form.postalCode">
                 </div>
               </div>
               <div class="p-2 w-full">
                 <div class="relative">
                   <label for="message" class="leading-7 text-sm text-gray-600">Message</label>
-                  <textarea id="message" name="message" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out" v-model="form.code"></textarea>
+                  <textarea required id="message" name="message" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out" v-model="form.code"></textarea>
                 </div>
               </div>
               <div class="p-2 w-full">
@@ -97,47 +93,3 @@
     </section>
   </main>
 </template>
-
-<script>
-  const encode = (data) => Object.keys(data)
-    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-    .join("&");
-
-  export default {
-    data() {
-      return {
-        hasSent: false,
-        form: {
-          name: '',
-          email: '',
-          code: '',
-          postalCode: '',
-        },
-      };
-    },
-    methods: {
-      sendForm() {
-        if (this.hasSent) {
-          return;
-        }
-        const options = {
-          method: 'POST',
-          headers: { "Content-Type": "application/x-www-form-urlencoded" },
-          body: encode(this.form),
-        };
-        fetch('/', options)
-        .then(() => {
-          this.hasSent = true;
-          this.form = {
-            name: '',
-            email: '',
-            code: '',
-            postalCode: '',
-          };
-        })
-        .catch(error => alert(error));
-      },
-    },
-  }
-</script>
-
